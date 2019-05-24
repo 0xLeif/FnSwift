a305e39213a680ee16ef3c02427db7f85b103f15
Zach 'Leif' Eriksen <zmeriksen@gmail.com>
2019-05-23 19:21:04 -0500

    Initial Commit

---
+++ FnSwift/FnSwift.h
@@ -0,0 +1,19 @@
+//
+//  FnSwift.h
+//  FnSwift
+//
+//  Created by Zach Eriksen on 5/23/19.
+//  Copyright © 2019 ol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FnSwift.
+FOUNDATION_EXPORT double FnSwiftVersionNumber;
+
+//! Project version string for FnSwift.
+FOUNDATION_EXPORT const unsigned char FnSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FnSwift/PublicHeader.h>
+
+
